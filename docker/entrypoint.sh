1dcc63cb3f7fc7f99379d3d5d168b761ca530fd2
Russell Edens <russell@voomify.com>
2022-05-17 11:15:29 -0400

    Re-added line to set the current date.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 sed -i "s/#{STITCH_CLIENT_ID}/$STITCH_CLIENT_ID/g" config.json
 sed -i "s/#{STITCH_TOKEN}/$STITCH_TOKEN/g" config.json
-#sed -i "s/#{EXCHANGE_RATE_START_DATE}/$(printf '%(%Y-%m-%d)T\n' -1)/g" config.usd.json
+sed -i "s/#{EXCHANGE_RATE_START_DATE}/$(printf '%(%Y-%m-%d)T\n' -1)/g" config.usd.json
 
 tap-exchangeratehost -c config.usd.json | target-stitch -c config.json
