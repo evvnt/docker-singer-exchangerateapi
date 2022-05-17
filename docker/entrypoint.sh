cfb921e7e56be2e9fd064364e0a6899b19e8a771
Russell Edens <russell@voomify.com>
2022-05-17 11:27:29 -0400

    Added -o pipefail to properly return error codes from tap-exchangeratehost.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -o pipefail
 sed -i "s/#{STITCH_CLIENT_ID}/$STITCH_CLIENT_ID/g" config.json
 sed -i "s/#{STITCH_TOKEN}/$STITCH_TOKEN/g" config.json
 sed -i "s/#{EXCHANGE_RATE_START_DATE}/$(printf '%(%Y-%m-%d)T\n' -1)/g" config.usd.json
